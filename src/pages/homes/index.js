587424b0d3d556420fc1b354035d95ee8feb9d6c
Leonardo geja <bemmenos@gmail.com>
2021-04-29 19:31:16 -0300

    criado arquivo service.js para consumir api

--- src/pages/homes/index.js
+++ src/pages/homes/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
+import './style.scss';
+export default function Home() {
 
-export default function home() {
+  const [filmes, setFimes] = useState([]);
+
+  useEffect(() => {
+    const loadFimes = async () => {
+      const response = await api.get('r-api/?api=filmes')
+      setFimes(response.data);
+    }
+    loadFimes();
+  }, [])
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className="container">
+      <section className="list-fimes">
+        {filmes.map((filme) => {
+          return (
+            <article>
+              <strong>{filme.nome}</strong>
+              <img src={filme.foto} alt={filme.nome} />
+            </article>
+          )
+        })}
+      </section>
+    </main>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-/* https://sujeitoprogramador.com/r-api/?api=filmes*/
+
 import Header from './components/header/'
 import Home from './pages/homes'
 export default function routes() {
